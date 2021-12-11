e70074809012865a96f0c3fd4d50a6cd0d121ef7
Serhii Osmolenko <osmolenko.s@gmail.com>
2021-12-11 19:45:08 +0200

    feat: home page button + text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Container, Fab, Typography } from '@mui/material';
 import GradientItem from '../components/GradientItem';
 import { connect } from 'react-redux';
+import { Add } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
 	render() {
 		const { list } = this.props;
 		return (
-			<Box
-				className="App"
-				sx={{
-					display: 'flex',
-					flexWrap: 'wrap',
-					justifyContent: 'space-around',
-				}}>
-				{list.map((gradient, index) => (
-					<GradientItem key={gradient.id} gradient={gradient} />
-				))}
-			</Box>
+			<>
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						flexDirection: 'column',
+						maxWidth: '400px',
+						margin: '15px auto',
+						textAlign: 'center',
+					}}>
+					<Typography variant="h4">Gradient lister and editor</Typography>
+					<Link to="/new">
+						<Fab
+							variant="extended"
+							color="primary"
+							aria-label="add new gradient">
+							<Add />
+							Add new gradient
+						</Fab>
+					</Link>
+				</Box>
+				<Container
+					className="App"
+					sx={{
+						display: 'flex',
+						flexWrap: 'wrap',
+						justifyContent: 'space-around',
+					}}>
+					{list.map((gradient, index) => (
+						<GradientItem key={gradient.id} gradient={gradient} />
+					))}
+				</Container>
+			</>
 		);
 	}
 }
