030ae3ea97db3bd5dc0fe1955b76cb2bd21085ad
Serhii Osmolenko <osmolenko.s@gmail.com>
2021-12-11 19:13:32 +0200

    feat: working form

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -12,143 +12,179 @@ import {
 	OutlinedInput,
 } from '@mui/material';
 import { Close } from '@mui/icons-material';
+import { connect } from 'react-redux';
+import {
+	addNewItem,
+	changeCurrent,
+	clearCurrent,
+	modifyItem,
+} from '../actions/gradient';
 
-const EditForm = () => {
-	const validationSchema = yup.object().shape({
-		name: yup.string('Enter correct name').min(8).required('Name is required'),
-		colors: yup
-			.array('Must be an array')
-			.min(2)
-			.of(
-				yup.object().shape({
-					id: yup.number(),
-					color: yup
-						.string()
-						.matches(
-							/^#([0-9a-f]{3}|[0-9a-f]{6})$/i,
-							'Must be a HEX color code'
-						)
-						.required('Color is required'),
-				})
-			),
-	});
+class EditForm extends React.Component {
+	componentDidMount() {
+		this.props.changeCurrent(this.props.id);
+	}
 
-	return (
-		<Formik
-			initialValues={{
-				name: 'New color',
-				colors: [
-					{
-						id: Math.random() * 100,
-						color: '#fff',
-					},
-					{
-						id: Math.random() * 100,
-						color: '#aeaeae',
-					},
-				],
-			}}
-			validationSchema={validationSchema}
-			onSubmit={(values) => {
-				alert(JSON.stringify(values));
-			}}>
-			{({ values, touched, errors, handleChange, handleBlur, isValid }) => {
-				const touchedName = getIn(touched, 'name');
-				const errorName = getIn(errors, 'name');
+	componentWillUnmount() {
+		this.props.clearCurrent();
+	}
 
-				return (
-					<Form noValidate autoComplete="off">
-						<FormControl fullWidth error={Boolean(touchedName && errorName)}>
-							<InputLabel htmlFor="name">Name</InputLabel>
-							<OutlinedInput
-								id="name"
-								name="name"
-								value={values.name}
-								onChange={handleChange}
-								label="Name"
-							/>
-							<FormHelperText>
-								{touchedName && errorName ? errorName : ''}
-							</FormHelperText>
-						</FormControl>
-						<Divider />
-						<FieldArray name="colors">
-							{({ push, remove }) => (
-								<>
-									{values.colors.map((c, index) => {
-										const currentColor = `colors[${index}].color`;
-										const touchedCurrentColor = getIn(touched, currentColor);
-										const errorCurrentColor = getIn(errors, currentColor);
+	render() {
+		const { current, id } = this.props;
+		const { navigate, addNewItem, modifyItem } = this.props;
 
-										return (
-											<FormControl
-												fullWidth
-												error={Boolean(
-													touchedCurrentColor && errorCurrentColor
-												)}
-												key={c.id}
-												margin="dense">
-												<InputLabel htmlFor={currentColor}>Color</InputLabel>
-												<OutlinedInput
-													name={currentColor}
-													value={c.color}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													label={currentColor}
-													endAdornment={
-														<InputAdornment position="end">
-															<IconButton
-																aria-label={`remove ${c.color} color from list`}
-																edge="end"
-																onClick={() => remove(index)}
-																sx={{
-																	display:
-																		values.colors.length <= 2
-																			? 'none'
-																			: 'inherit',
-																}}>
-																<Close color="error" />
-															</IconButton>
-														</InputAdornment>
-													}
-												/>
-												<FormHelperText>
-													{touchedCurrentColor && errorCurrentColor
-														? errorCurrentColor
-														: ''}
-												</FormHelperText>
-											</FormControl>
-										);
-									})}
-									<Button
-										type="button"
-										variant="outlined"
-										onClick={() =>
-											push({
-												id: Math.random(),
-												color: '#aeaeae',
-											})
-										}
-										sx={{
-											marginRight: '10px',
-										}}>
-										Add 1 more color
-									</Button>
-								</>
-							)}
-						</FieldArray>
-						<Button
-							type="submit"
-							color="primary"
-							variant="contained"
-							disabled={!isValid || values.colors.length === 0}>
-							submit
-						</Button>
-					</Form>
-				);
-			}}
-		</Formik>
-	);
-};
+		const initial = {
+			id: Math.random() * 100,
+			name: 'New color',
+			colors: [
+				{
+					id: Math.random() * 100,
+					color: '#fff',
+				},
+				{
+					id: Math.random() * 100,
+					color: '#aeaeae',
+				},
+			],
+		};
 
-export default EditForm;
+		const validationSchema = yup.object().shape({
+			name: yup
+				.string('Enter correct name')
+				.min(2)
+				.required('Name is required'),
+			colors: yup
+				.array('Must be an array')
+				.min(2)
+				.of(
+					yup.object().shape({
+						id: yup.number(),
+						color: yup
+							.string()
+							.matches(
+								/^#([0-9a-f]{3}|[0-9a-f]{6})$/i,
+								'Must be a HEX color code'
+							)
+							.required('Color is required'),
+					})
+				),
+		});
+
+		return (
+			<Formik
+				initialValues={current || initial}
+				validationSchema={validationSchema}
+				onSubmit={(values) => {
+					if (id) {
+						modifyItem(values);
+					} else {
+						addNewItem(values);
+					}
+					navigate();
+				}}
+				enableReinitialize>
+				{({ values, touched, errors, handleChange, handleBlur, isValid }) => {
+					const touchedName = getIn(touched, 'name');
+					const errorName = getIn(errors, 'name');
+					return (
+						<Form noValidate autoComplete="off">
+							<FormControl fullWidth error={Boolean(touchedName && errorName)}>
+								<InputLabel htmlFor="name">Name</InputLabel>
+								<OutlinedInput
+									id="name"
+									name="name"
+									value={values.name}
+									onChange={handleChange}
+									label="Name"
+								/>
+								<FormHelperText>
+									{touchedName && errorName ? errorName : ''}
+								</FormHelperText>
+							</FormControl>
+							<Divider />
+							<FieldArray name="colors">
+								{({ push, remove }) => (
+									<>
+										{values.colors.map((c, index) => {
+											const currentColor = `colors[${index}].color`;
+											const touchedCurrentColor = getIn(touched, currentColor);
+											const errorCurrentColor = getIn(errors, currentColor);
+
+											return (
+												<FormControl
+													fullWidth
+													error={Boolean(
+														touchedCurrentColor && errorCurrentColor
+													)}
+													key={c.id}
+													margin="dense">
+													<InputLabel htmlFor={currentColor}>Color</InputLabel>
+													<OutlinedInput
+														name={currentColor}
+														value={c.color}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														label={currentColor}
+														endAdornment={
+															<InputAdornment position="end">
+																<IconButton
+																	aria-label={`remove ${c.color} color from list`}
+																	edge="end"
+																	onClick={() => remove(index)}
+																	sx={{
+																		display:
+																			values.colors.length <= 2
+																				? 'none'
+																				: 'inherit',
+																	}}>
+																	<Close color="error" />
+																</IconButton>
+															</InputAdornment>
+														}
+													/>
+													<FormHelperText>
+														{touchedCurrentColor && errorCurrentColor
+															? errorCurrentColor
+															: ''}
+													</FormHelperText>
+												</FormControl>
+											);
+										})}
+										<Button
+											type="button"
+											variant="outlined"
+											onClick={() =>
+												push({
+													id: Math.random(),
+													color: '#aeaeae',
+												})
+											}
+											sx={{
+												marginRight: '10px',
+											}}>
+											Add 1 more color
+										</Button>
+									</>
+								)}
+							</FieldArray>
+							<Button
+								type="submit"
+								color="primary"
+								variant="contained"
+								disabled={!isValid || values.colors.length === 0}>
+								{id ? 'modify' : 'submit'}
+							</Button>
+						</Form>
+					);
+				}}
+			</Formik>
+		);
+	}
+}
+
+export default connect(
+	({ gradients: { current } }) => ({
+		current,
+	}),
+	{ changeCurrent, clearCurrent, addNewItem, modifyItem }
+)(EditForm);
--- src/pages/EditGradient.jsx
+++ src/pages/EditGradient.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import EditForm from '../components/EditForm';
+import { useNavigate, useParams } from 'react-router-dom';
 
-export default class NewGradient extends React.Component {
-	render() {
-		return (
-			<Box
-				className="App"
+function EditGradient(props) {
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	return (
+		<Box
+			className="App"
+			sx={{
+				padding: '20px',
+				maxWidth: '600px',
+				margin: '0 auto',
+			}}>
+			<Typography
+				variant="h3"
 				sx={{
-					padding: '20px',
-					maxWidth: '600px',
-					margin: '0 auto',
+					textAlign: 'center',
 				}}>
-				<Typography
-					variant="h3"
-					sx={{
-						textAlign: 'center',
-					}}>
-					Add new gradient
-				</Typography>
-				<EditForm />
-			</Box>
-		);
-	}
+				{!id ? 'Add new gradient' : 'Edit gradient'}
+			</Typography>
+			<EditForm id={id} navigate={() => navigate('/')} />
+		</Box>
+	);
 }
+
+export default EditGradient;
